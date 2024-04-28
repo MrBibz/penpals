03b598875166bc2cf234e7ae0fdfa9979f94c764
mrbibz <blanco.bibeau@gmail.com>
2024-04-28 11:24:53 -0400

    Initial commit

---
+++ src/main/java/org/mrbibz/penpals/PenpalsApplication.java
@@ -0,0 +1,13 @@
+package org.mrbibz.penpals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenpalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenpalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mrbibz/penpals/PenpalsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrbibz.penpals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenpalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
